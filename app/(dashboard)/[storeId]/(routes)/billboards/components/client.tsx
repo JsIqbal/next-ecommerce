91372a3378bb9d984bdf4fe8db8c402d933c5f89
jsKraken <javascriptiqbal@gmail.com>
2023-08-03 19:54:53 +0600

    Complete Billboard Feature

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
     data: BillboardColumn[];
@@ -35,6 +36,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
             </div>
             <Separator />
             <DataTable searchKey="label" columns={columns} data={data} />
+            <Heading title="API" description="API calls for Billboards" />
+            <Separator />
+            <ApiList entityName="billboards" entityIdName="billboardId" />
         </>
     );
 };
--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 
-export default authMiddleware({});
+export default authMiddleware({
+    publicRoutes: ["/api/:path*"], // to access all get routes and public routes without authentication
+});
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
