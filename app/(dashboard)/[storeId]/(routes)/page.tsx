fec83944782ed8f8ecb8f8157824c9f23a548d26
Ahmad Munab <ahmadmunab22@gmail.com>
2023-08-08 23:04:39 +0600

    added dashboard heading

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,16 +1,36 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
+import { DollarSign } from "lucide-react";
 
 interface DashboardPageProps {
     params: { storeId: string };
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId,
-        },
-    });
-    return <div>Active Store: {store?.name}</div>;
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading title="Dashboard" description="Complete overview of your store"/>
+                <Separator/>
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 p-2">
+                            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-forground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                100
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default DashboardPage;
